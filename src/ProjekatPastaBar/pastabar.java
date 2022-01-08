9f8ce050fecd35098cd684e7d8751948e0ba14f0
Zeljko Manojlovic <zeljkom83@yahoo.com>
2022-01-08 19:21:00 +0100

    Initial commit

---
+++ src/ProjekatPastaBar/pastabar.java
@@ -0,0 +1,20 @@
+package ProjekatPastaBar;
+
+public class pastabar {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		
+		String[] sastojci = { "Makarone", "Spagete", "Bolognese", "Curetina", "Govedja prsuta", "Slanina", "Piletina",
+				"4 sira", "Dimljeni sir", "Parmezan", "Pavlaka", "Pesto sos", "Napolitana", "Povrce mix", "Pecurke",
+				"Kutija" };
+		int[] cene = { 50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20 };
+
+		String[] stalneMusterije = { "0631111111", "063222222", "063333333", "064444444", "065555555", "066666666" };
+		
+		
+	}
+
+}
